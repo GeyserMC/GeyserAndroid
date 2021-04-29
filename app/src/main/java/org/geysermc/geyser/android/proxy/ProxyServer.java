050b98db379c372ab11eb2989399bc38e0951179
Camotoy <20743703+Camotoy@users.noreply.github.com>
2021-04-29 15:38:04 -0400

    Update Bedrock proxy supported version

--- app/src/main/java/org/geysermc/geyser/android/proxy/ProxyServer.java
+++ app/src/main/java/org/geysermc/geyser/android/proxy/ProxyServer.java
@@ -32,7 +32,7 @@ import com.nukkitx.protocol.bedrock.BedrockPong;
 import com.nukkitx.protocol.bedrock.BedrockServer;
 import com.nukkitx.protocol.bedrock.BedrockServerEventHandler;
 import com.nukkitx.protocol.bedrock.BedrockServerSession;
-import com.nukkitx.protocol.bedrock.v428.Bedrock_v428;
+import com.nukkitx.protocol.bedrock.v431.Bedrock_v431;
 
 import org.geysermc.geyser.android.R;
 import org.geysermc.geyser.android.utils.EventListeners;
@@ -52,7 +52,7 @@ import lombok.Getter;
 
 public class ProxyServer {
 
-    public static final BedrockPacketCodec CODEC = Bedrock_v428.V428_CODEC;
+    public static final BedrockPacketCodec CODEC = Bedrock_v431.V431_CODEC;
 
     private BedrockServer bdServer;
     private BedrockPong bdPong;
